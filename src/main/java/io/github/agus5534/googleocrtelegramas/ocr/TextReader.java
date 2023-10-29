90550d1258975d444f6aeae7c11ebe8a98afb680
Agus5534 <agustinmonte06@gmail.com>
2023-10-29 17:28:59 -0300

    asd

--- src/main/java/io/github/agus5534/googleocrtelegramas/ocr/TextReader.java
+++ src/main/java/io/github/agus5534/googleocrtelegramas/ocr/TextReader.java
@@ -8,6 +8,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class TextReader {
@@ -23,6 +24,7 @@ public class TextReader {
                 AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
         requests.add(request);
 
+
         try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
             BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
             List<AnnotateImageResponse> responses = response.getResponsesList();
@@ -33,10 +35,52 @@ public class TextReader {
                     return;
                 }
 
+                HashMap<List<List<Integer>>, EntityAnnotation> test = new HashMap<>();
+
                 for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
-                    System.out.format("Text: %s%n", annotation.getDescription());
-                    System.out.format("Position : %s%n", annotation.getBoundingPoly());
+
+                    BoundingPoly boundingPoly = annotation.getBoundingPoly();
+                    List<Vertex> vertices = boundingPoly.getVerticesList();
+
+                    List<List<Integer>> vertexList = new ArrayList<>();
+
+                    for(var vertex : vertices) {
+                        vertexList.add(List.of(vertex.getX(), vertex.getY()));
+                    }
+
+                    test.put(vertexList, annotation);
+
+                    /*System.out.format("Text: %s%n", annotation.getDescription());
+                    System.out.format("Position : %s%n", annotation.getBoundingPoly());*/
                 }
+
+                test.forEach((lists, entityAnnotation) -> {
+                    var isVotoLLA = lists.equals(
+                            List.of(
+                                    List.of(1245, 928),
+                                    List.of(1348, 935),
+                                    List.of(1345, 969),
+                                    List.of(1243, 962)
+                            )
+                    );
+
+                    var isVotoUXP = lists.equals(
+                            List.of(
+                                    List.of(1232, 999),
+                                    List.of(1340, 1002),
+                                    List.of(1339, 1046),
+                                    List.of(1231, 1043)
+                            )
+                    );
+
+                    if(isVotoLLA) {
+                        System.out.println("Votos LLA: " + entityAnnotation.getDescription());
+                    }
+
+                    if(isVotoUXP) {
+                        System.out.println("Votos UxP: " + entityAnnotation.getDescription());
+                    }
+                });
             }
         }
     }
