d21dae791cc3f5c52e5da6a4767580250dda831e
Agus5534 <agustinmonte06@gmail.com>
2023-10-30 20:22:14 -0300

    WHY YOU DIDN'T UPLOAD >:C

--- src/main/java/io/github/agus5534/googleocrtelegramas/Main.java
+++ src/main/java/io/github/agus5534/googleocrtelegramas/Main.java
@@ -1,13 +1,41 @@
 package io.github.agus5534.googleocrtelegramas;
 
+import com.google.common.primitives.Bytes;
 import io.github.agus5534.googleocrtelegramas.utils.FileCreator;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static FileCreator mainFolder = new FileCreator(new File(System.getProperty("user.home")), "elecciones-tests/");
     public static void main(String[] args) {
-        TelegramaDownloader.download("0200700620X");
+
+        byte[] bytes;
+        try {
+            var url = new URL("https://cdn.discordapp.com/attachments/967975761543839764/1168685619573174432/balotaje.txt").openStream();
+
+
+            var string = IOUtils.toString(url, StandardCharsets.UTF_8);
+            var stringArray = string.replace("[","").replace("]", "").replace(" ", "").split(",");
+
+            bytes = new byte[stringArray.length];
+
+            for(int i = 0; i < stringArray.length; i++) {
+               bytes[i] = Byte.parseByte(stringArray[i]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        TelegramaDownloader.download(bytes);
     }
 }
