7726c7194b5d8bb29858e1f4dde684e0d8e1b15a
MartinDiCe <jamartindice@gmail.com>
2023-10-30 10:35:35 -0300

    Update Final para dev

--- src/main/java/io/github/agus5534/googleocrtelegramas/Main.java
+++ src/main/java/io/github/agus5534/googleocrtelegramas/Main.java
@@ -1,24 +1,13 @@
 package io.github.agus5534.googleocrtelegramas;
 
-import io.github.agus5534.googleocrtelegramas.services.ImgDownloader;
 import io.github.agus5534.googleocrtelegramas.utils.FileCreator;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
 
 public class Main {
 
     public static FileCreator mainFolder = new FileCreator(new File(System.getProperty("user.home")), "elecciones-tests/");
-    public static void main(String[] args) throws IOException {
-        ImgDownloader imgDownloader = new ImgDownloader("https://i.imgur.com/8uUPupv.jpg");
-
-        BufferedImage image = imgDownloader.downloadImage();
-
-        byte[] imageBytes = imgDownloader.downloadImageBytes();
-
-        String imageBase64 = imgDownloader.downloadImageBase64();
-
+    public static void main(String[] args) {
+        TelegramaDownloader.download("0200700680X");
     }
 }
