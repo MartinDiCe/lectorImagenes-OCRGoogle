e1431bfed78f8f7d109b83a63b8ff80383ff34c9
MartinDiCe <jamartindice@gmail.com>
2023-11-07 10:19:26 -0300

    Fix convertidor de caracter a números y optimización de la clase. Creador de concatenador de strings proximos por distancia y cantidad de caracteres. Optimización de texreader

--- src/main/java/io/github/agus5534/googleocrtelegramas/utils/texts/StringToNumberConverter.java
+++ src/main/java/io/github/agus5534/googleocrtelegramas/utils/texts/StringToNumberConverter.java
@@ -1,5 +1,8 @@
 package io.github.agus5534.googleocrtelegramas.utils.texts;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringToNumberConverter {
 
     public static int convert(String value) {
@@ -16,45 +19,28 @@ public class StringToNumberConverter {
     private static int customStringToNumber(String value) {
         String lowerCaseValue = value.toLowerCase();
         StringBuilder result = new StringBuilder();
+        boolean foundMinusOne = false;
 
         for (char character : lowerCaseValue.toCharArray()) {
-            switch (character) {
-                case 'o', 'c':
-                    result.append('0');
-                    break;
-                case 'l', 'n', 'i', '|', '/':
-                    result.append('1');
-                    break;
-                case 'z':
-                    result.append('2');
-                    break;
-                case 'e', 'b':
-                    result.append('6');
-                    break;
-                case 'a', 'y', 'v', 'h', 'u', 'k', '\u0446', '\u0438':
-                    result.append('4');
-                    break;
-                case 's':
-                    result.append('5');
-                    break;
-                case 'g', 'q', 'p':
-                    result.append('9');
-                    break;
-                case 'B':
-                    result.append("13");
-                    break;
-                case '?', '\u0E32', '\u3131':
-                    result.append('7');
-                    break;
+            if (foundMinusOne) {
+                return -1;
+            }
 
-                default:
+            if (Character.isDigit(character)) {
+                result.append(character);
+            } else {
+                Integer mappedValue = charToNumberMap.get(character);
+                if (mappedValue != null) {
+                    result.append(mappedValue);
+                } else {
                     result.append("-1");
-                    break;
+                    foundMinusOne = true;
+                }
             }
         }
-
         return Integer.parseInt(result.toString());
     }
+
 
     private static boolean isNumeric(String text) {
         try {
@@ -63,6 +49,39 @@ public class StringToNumberConverter {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    private static final Map<Character, Integer> charToNumberMap = new HashMap<>();
+    static {
+        charToNumberMap.put('o', 0);
+        charToNumberMap.put('c', 0);
+        charToNumberMap.put('l', 1);
+        charToNumberMap.put('n', 1);
+        charToNumberMap.put('i', 1);
+        charToNumberMap.put('|', 1);
+        charToNumberMap.put('(', 1);
+        charToNumberMap.put('z', 2);
+        charToNumberMap.put('e', 6);
+        charToNumberMap.put('b', 6);
+        charToNumberMap.put('G', 6);
+        charToNumberMap.put('a', 4);
+        charToNumberMap.put('y', 4);
+        charToNumberMap.put('v', 4);
+        charToNumberMap.put('h', 4);
+        charToNumberMap.put('u', 4);
+        charToNumberMap.put('k', 4);
+        charToNumberMap.put('\u0446', 4);
+        charToNumberMap.put('\u0438', 4);
+        charToNumberMap.put('s', 5);
+        charToNumberMap.put('g', 9);
+        charToNumberMap.put('q', 9);
+        charToNumberMap.put('p', 9);
+        charToNumberMap.put('B', 13);
+        charToNumberMap.put('?', 7);
+        charToNumberMap.put('\u0E32', 7);
+        charToNumberMap.put('\u3131', 7);
+        charToNumberMap.put('+', 7);
+        charToNumberMap.put('&', 8);
     }
 
 }
